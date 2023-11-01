12ccf8bb5a23752831b2b2ea8a6af66477e8cfc1
[200~git config --global user.name FIRST_NAME <ferjanikhouloud@gmail.com>
2023-11-01 18:37:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'John Doe',
+        bio: 'I am a developer',
+        imgSrc: 'image.jpg',
+        profession: 'Software Engineer',
+      },
+      show: false,
+      timeSinceMount: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState((prevState) => ({
+        timeSinceMount: prevState.timeSinceMount + 1,
+      }));
+    }, 1000); // Update every second
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, timeSinceMount } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+        {show && (
+          <div>
+            <img src={imgSrc} alt={fullName} />
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <p>{profession}</p>
+          </div>
+        )}
+        <p>Time since component mounted: {timeSinceMount} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
